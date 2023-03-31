a3d363128f1e1c35b93ae0f8f0132421072f746b
sunnysh17 <123319227+sunnysh17@users.noreply.github.com>
2023-03-31 21:29:31 +0530

    Added starter Files

---
+++ app.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>I am H1 Tag</h1>
+ *          <h2>I am H1 Tag</h2> Sibling
+ *      </div>
+ *      <div id="child2">
+ *          <h1>I am H1 Tag</h1>
+ *          <h2>I am H1 Tag</h2> Sibling
+ *      </div>
+ * </div>
+ * ReactElement(Object) => HTML(Browser Understand)
+ */
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    [React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h2",{},"I am h2 tag"),
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h2",{},"I am h2 tag"),
+    ]) //To add more than one sibling use array
+]);
+
+const heading = React.createElement(
+    "h1",
+    {id:"heading"},
+    "Hello From React"
+); //create h1 react element
+
+console.log(parent); //returns object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);// take object, convert into heading tag and put inside root
