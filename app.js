1b3323e00b00fe4459b36d659f10702e1a1c7bdb
sunny <2022.sunny.halkatti@ves.ac.in>
2023-04-01 22:49:39 +0530

    episode 2 finished

--- app.js
+++ app.js
@@ -12,13 +12,14 @@
  * </div>
  * ReactElement(Object) => HTML(Browser Understand)
  */
-
+import React from "react";
+import ReactDOM  from "react-dom/client";
 const parent = React.createElement(
     "div",
     {id:"parent"},
     [React.createElement("div",{id:"child"},[
         React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"I am h2 tag"),
+        React.createElement("h2",{},"I am Sunny"),
     ]),
     React.createElement("div",{id:"child2"},[
         React.createElement("h1",{},"I am h1 tag"),
