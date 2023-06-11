af8cbbc1278ecb0245355ccf14d854da785e39b5
sunny <2022.sunny.halkatti@ves.ac.in>
2023-06-11 10:47:01 +0530

    props ep4

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const HeaderComponent = () =>{
     );
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+    const {resData} = props;
     return (
         <div className="res-card" style={{ backgroundColor:"#f0f0f0" }}>
             <img 
@@ -28,12 +29,147 @@ const RestaurantCard = () => {
             alt="res-logo"
             src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/p9fv8plp2ievunbvvfge" 
             />
-            <h3>Meghana Foods</h3>
-            <h4>North Indian, Asian</h4>
-            <h4>4.5 stars</h4>
-            <h4>38 minutes</h4>
+            <h3>{resData.data.name}</h3>
+            <h4>{resData.data.cuisines.join(", ")}</h4>
+            <h4>{resData.data.avgRating} stars</h4>
+            <h4>₹{resData.data.costForTwo/100} FOR TWO</h4>
+            <h4>{resData.data.deliveryTime} minutes</h4>
         </div>
     );
+};
+
+const resObj = {
+    "type": "restaurant",
+                "data": {
+                  "type": "F",
+                  "id": "25610",
+                  "name": "Balaji Food Court",
+                  "uuid": "f752387f-7752-4938-9666-cbc2c582f6c4",
+                  "city": "5",
+                  "area": "Thane Panchpakhadi",
+                  "totalRatingsString": "10000+ ratings",
+                  "cloudinaryImageId": "mx2xvec1dkyu8yxor1ml",
+                  "cuisines": [
+                    "North Indian",
+                    "Chinese",
+                    "Juices",
+                    "Continental",
+                    "Ice Cream",
+                    "South Indian"
+                  ],
+                  "tags": [
+                    
+                  ],
+                  "costForTwo": 25000,
+                  "costForTwoString": "₹250 FOR TWO",
+                  "deliveryTime": 40,
+                  "minDeliveryTime": 40,
+                  "maxDeliveryTime": 40,
+                  "slaString": "40 MINS",
+                  "lastMileTravel": 3.700000047683716,
+                  "slugs": {
+                    "restaurant": "balaji-food-court-panch-pakhadi-thane-west-thane",
+                    "city": "mumbai"
+                  },
+                  "cityState": "5",
+                  "address": "Shop 4 & 5, Sital Premises, Panch Pakhadi, Thane West",
+                  "locality": "Panch Pakhadi",
+                  "parentId": 40639,
+                  "unserviceable": false,
+                  "veg": false,
+                  "select": false,
+                  "favorite": false,
+                  "tradeCampaignHeaders": [
+                    
+                  ],
+                  "ribbon": [
+                    {
+                      "type": "PROMOTED"
+                    }
+                  ],
+                  "chain": [
+                    
+                  ],
+                  "feeDetails": {
+                    "fees": [
+                      {
+                        "name": "distance",
+                        "fee": 3500,
+                        "message": ""
+                      },
+                      {
+                        "name": "time",
+                        "fee": 0,
+                        "message": ""
+                      },
+                      {
+                        "name": "special",
+                        "fee": 0,
+                        "message": ""
+                      }
+                    ],
+                    "totalFees": 3500,
+                    "message": "",
+                    "title": "Delivery Charge",
+                    "amount": "3500",
+                    "icon": ""
+                  },
+                  "availability": {
+                    "opened": true,
+                    "nextOpenMessage": "",
+                    "nextCloseMessage": ""
+                  },
+                  "longDistanceEnabled": 0,
+                  "rainMode": "NONE",
+                  "thirdPartyAddress": false,
+                  "thirdPartyVendor": "",
+                  "adTrackingID": "cid=6900722~p=1~eid=00000188-a8cd-5ca8-10c0-9063004c0148",
+                  "badges": {
+                    "imageBased": [
+                      
+                    ],
+                    "textBased": [
+                      
+                    ],
+                    "textExtendedBadges": [
+                      
+                    ]
+                  },
+                  "lastMileTravelString": "3.7 kms",
+                  "hasSurge": false,
+                  "aggregatedDiscountInfoV3": {
+                    "header": "20% OFF",
+                    "subHeader": "UPTO ₹50",
+                    "discountTag": "",
+                    "headerTypeV2": 0
+                  },
+                  "loyaltyDiscoverPresentationInfo": {
+                    "logoCtx": {
+                      "text": "BENEFITS",
+                      "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+                    },
+                    "freedelMessage": "FREE DELIVERY",
+                    "exclusiveOfferMessage": ""
+                  },
+                  "sla": {
+                    "restaurantId": "25610",
+                    "deliveryTime": 40,
+                    "minDeliveryTime": 40,
+                    "maxDeliveryTime": 40,
+                    "lastMileTravel": 3.700000047683716,
+                    "lastMileDistance": 0,
+                    "serviceability": "SERVICEABLE",
+                    "rainMode": "NONE",
+                    "longDistance": "NOT_LONG_DISTANCE",
+                    "preferentialService": false,
+                    "iconType": "EMPTY"
+                  },
+                  "promoted": true,
+                  "avgRating": "4.2",
+                  "totalRatings": 10000,
+                  "new": false
+                },
+                "subtype": "basic"
 };
 
 const Body = () => {
@@ -42,18 +178,7 @@ const Body = () => {
             <div className="search">Search</div>
             <div className="res-container">
                 {/* this will contain restro cards */}
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
+                <RestaurantCard resData={resObj}/>
             </div>
         </div>
 
