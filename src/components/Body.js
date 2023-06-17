7cd5c518f3b414cc4754f293c5e2aabe554ddab1
sunny <2022.sunny.halkatti@ves.ac.in>
2023-06-17 22:01:32 +0530

    Ep5 Completed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import {useState} from "react";
 
 const Body = () => {
+    //Creating a Local State Variables
+    const [listOfRestaurants, setListOfRestaurant] = useState(resList);
+
     return (
         <div className="body">
+            
             <div className="filter">
                 <button 
                     className="filter-btn" 
-                    onClick={() => {console.log("Btn clicked")}}>
+                    onClick={() => {
+                        //Filter logic here - Displaying restro with rating > 4
+                        const filteredList = listOfRestaurants.filter(
+                                (res) => res.data.avgRating > 4
+                            );
+                        setListOfRestaurant(filteredList);
+                    }}>
                     Top Rated Restaurants
                 </button>
             </div>
+
             <div className="res-container">
                 {/* this will contain restro cards */}
-                {resList.map((restaurant) => (
+                {listOfRestaurants.map((restaurant) => (
                     <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
                 ))}
             </div>
